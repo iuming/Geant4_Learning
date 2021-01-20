dfedcef04c1320cd5c6a4d9fc95ad372349792b3
liuming <6ming@hrbeu.edu.cn>
2021-01-20 21:31:40 +0800

    add some code in Ex01.cpp

--- example/Ex01/Ex01.cpp
+++ example/Ex01/Ex01.cpp
@@ -1,1 +1,11 @@
 #include "G4RunManager.hh"
+#include "G4UImanager.hh"
+
+#include "DetectorConstruction.hh"
+#include "PhysicalList.hh"
+#include "PrimaryGeneratorAction.hh"
+
+int main(){
+    G4RunManager * runManager = new G4RunManager;
+    runManager->SetUserInitialization(new DetectorConstruction);
+}
