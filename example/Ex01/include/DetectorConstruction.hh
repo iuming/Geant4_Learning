951172e772efcec5e2ed1556daade9d6aace5339
iuming <6ming@hrbeu.edu.cn>
2021-02-09 15:37:08 +0800

    add DetectorConstruction notes

--- example/Ex01/include/DetectorConstruction.hh
+++ example/Ex01/include/DetectorConstruction.hh
@@ -1,20 +1,21 @@
 #ifndef DetectorConstruction_h
 #define DetectorConstruction_h 1
 
-#include "G4VUserDetectorConstruction.hh"
-#include "globals.hh"
+#include "G4VUserDetectorConstruction.hh"   // Geant4为用户提供的探测器结构类
+#include "globals.hh"                       // 包含Geant4中的全局变量等，除Actioninitializtion外均应包含
 
-class G4PhysicalVolume;
-class G4LogicalVolume;
+class G4PhysicalVolume;                     // Geant4物理体类
+class G4LogicalVolume;                      // Geant4逻辑体类
 
-class DetectorConstruction : public G4VUserDetectorConstruction
+// 定义探测器结构类，继承G4探测器结构类
+class DetectorConstruction : public G4VUserDetectorConstruction 
 {
 private:
-    void DefineMaterials();
+    void DefineMaterials();                 // 定义材料，仅在类中可进行调用
 public:
-    DetectorConstruction();
-    virtual ~DetectorConstruction();
-    virtual G4VPhysicalVolume* Construct();
+    DetectorConstruction();                 // 定义探测器材料
+    virtual ~DetectorConstruction();        // 释放内存（可为空）
+    virtual G4VPhysicalVolume* Construct(); // 定义探测器结构
 }; 
 
 #endif
--- example/Ex01/src/DetectorConstruction.cpp
+++ example/Ex01/src/DetectorConstruction.cpp
@@ -1,37 +1,41 @@
-#include "DetectorConstruction.hh"
+#include "DetectorConstruction.hh"  
 
-#include "G4NistManager.hh"
-#include "G4Box.hh"
-#include "G4LogicalVolume.hh"
-#include "G4PVPlacement.hh"
-#include "G4RotationMatrix.hh"
-#include "G4Transform3D.hh"
-#include "G4PhysicalConstants.hh"
-#include "G4SystemOfUnits.hh"
+#include "G4NistManager.hh"            // Geant4材料库
+#include "G4Box.hh"                    // Geant4方形物体
+#include "G4LogicalVolume.hh"          // Geant4逻辑体
+#include "G4PVPlacement.hh"            // Geant4相对位置
+#include "G4RotationMatrix.hh"         // Geant4旋转角度
+#include "G4Transform3D.hh"            // Geant4相对变换
+#include "G4PhysicalConstants.hh"      // Geant4物理体
+#include "G4SystemOfUnits.hh"          // Geant4单位制
 
+// 构造函数
 DetectorConstruction::DetectorConstruction():G4VUserDetectorConstruction(){
    DefineMaterials();
 }
 
+// 析构函数
 DetectorConstruction::~DetectorConstruction(){
 
 }
 
+// 定义物质材料
 void DetectorConstruction::DefineMaterials(){
-   G4NistManager* man = G4NistManager::Instance();
-   G4bool isotopes = false;
-   G4Element* H = man -> FindOrBuildElement("H", isotopes);
-   G4Element* O = man -> FindOrBuildElement("O", isotopes);
-   G4Material* H2O = new G4Material("Water", 1.000 * g/cm3, 2);
-   H2O -> AddElement(H, 2);
-   H2O -> AddElement(O, 1);
-   G4Element* N = man -> FindOrBuildElement("N", isotopes);
-   G4double density = 1.290 * mg/cm3;
-   G4Material* Air = new G4Material("Air", density, 2);
-   Air -> AddElement(N, 70 * perCent);
-   Air -> AddElement(O, 30 * perCent);
-}
+   G4NistManager* man = G4NistManager::Instance();                // 创建材料对象
+   G4bool isotopes = false;                                       // 设置同位素布尔量
+   G4Element* H = man -> FindOrBuildElement("H", isotopes);       // 从Geant4材料库找寻H
+   G4Element* O = man -> FindOrBuildElement("O", isotopes);       // 从Geant4材料库找寻O
+   G4Material* H2O = new G4Material("Water", 1.000 * g/cm3, 2);   // 创建H2O材料
+   H2O -> AddElement(H, 2);                                       // 定义H2O
+   H2O -> AddElement(O, 1);                                       // 定义H2O
+   G4Element* N = man -> FindOrBuildElement("N", isotopes);       // 从Geant4材料库找寻N
+   G4double density = 1.290 * mg/cm3;                             // 定义密度
+   G4Material* Air = new G4Material("Air", density, 2);           // 创建Air材料
+   Air -> AddElement(N, 70 * perCent);                            // 定义Air
+   Air -> AddElement(O, 30 * perCent);                            // 定义Air
+}           
 
+// 最重要的函数，设置探测器几何体、逻辑体、物理体
 G4VPhysicalVolume* DetectorConstruction::Construct(){
    G4NistManager* man = G4NistManager::Instance();
    G4Material* default_mat = man -> FindOrBuildMaterial("Air");
