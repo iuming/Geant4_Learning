fd74eaca043abdf01cc0f1af2df32087db013641
liuming <6ming@hrbeu.edu.cn>
2021-03-04 15:35:37 +0800

    add PhysicsList code

---
+++ example/Ex02/include/PhysicsList.hh
@@ -0,0 +1,15 @@
+#ifndef PhysicsList_h
+#define PhysicsList_h 1
+
+#include "G4VModularPhysicsList.hh"
+
+class PhysicsList : public G4VModularPhysicsList
+{
+    public:
+        PhysicsList();
+        ~PhysicsList();
+
+        virtual void SetCut();
+};
+
+#endif
