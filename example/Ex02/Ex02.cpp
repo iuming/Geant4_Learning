16f8670a6f606cbc0eca1225a81669e54e42594a
iuming <6ming@hrbeu.edu.cn>
2021-04-19 04:53:03 -0400

    添加一些笔记

--- example/Ex02/Ex02.cpp
+++ example/Ex02/Ex02.cpp
@@ -1,28 +1,29 @@
-#include "G4RunManager.hh"
-#include "G4UImanager.hh"
-#include "G4VisExecutive.hh"
-#include "G4UIExecutive.hh"
+#include "G4RunManager.hh"     //Geant4运行管理类
+#include "G4UImanager.hh"      //Geant4UI管理类
+#include "G4VisExecutive.hh"   //Geant4可视化类
+#include "G4UIExecutive.hh"    //Geant4UI执行类
 
-#include "DetectorConstruction.hh"
-#include "PhysicsList.hh"
-#include "PrimaryGeneratorAction.hh"
+#include "DetectorConstruction.hh"    //自己定义的探测器结构类
+#include "PhysicsList.hh"             //自己定义的物理过程类
+#include "PrimaryGeneratorAction.hh"  //自己定义的粒子发射器行为类
 
-int main(int argc, char** argv){
-    G4RunManager* runManager = new G4RunManager;
-    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-    G4VisManager* visManager = new G4VisExecutive;
-    visManager -> Initialize();
-    runManager -> SetUserInitialization(new DetectorConstruction);
-    runManager -> SetUserInitialization(new PhysicsList);
-    runManager -> SetUserAction(new PrimaryGeneratorAction);
-    runManager -> Initialize();
-    G4UImanager* UI = G4UImanager::GetUIpointer();
+int main(int argc, char** argv)
+{
+    G4RunManager* runManager = new G4RunManager;             //创建一个运行管理对象
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);       //创建一个UI执行对象
+    G4VisManager* visManager = new G4VisExecutive;           //创建一个可视化管理对象
+    visManager -> Initialize();                              //初始化可视化管理对象
+    runManager -> SetUserInitialization(new DetectorConstruction);  //将几何结构初始化至运行管理对象
+    runManager -> SetUserInitialization(new PhysicsList);           //将物理过程初始化运行管理对象
+    runManager -> SetUserAction(new PrimaryGeneratorAction);        //将初级粒子发射器行为类初始化运行管理对象
+    runManager -> Initialize();                                     //初始化运行管理对象
+    G4UImanager* UI = G4UImanager::GetUIpointer();                  //可视化操作
     UI -> ApplyCommand("/control/execute init_vis.mac");
     ui -> SessionStart();
     delete ui;
-    int numberOfEvents = 3;
-    runManager -> BeamOn(numberOfEvents);
-    delete runManager;
-    delete visManager;
+    int numberOfEvents = 3;                                       //设置发射粒子数
+    runManager -> BeamOn(numberOfEvents);                         //发射粒子
+    delete runManager;                                            //释放内存
+    delete visManager;                                            //释放内存
     return 0;
 }
